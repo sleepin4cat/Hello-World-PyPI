2204fb0516dcb274c900b18865f8ad4df36e62f4
Sleeping Cat <116789050+sleepin4cat@users.noreply.github.com>
2022-12-17 13:02:09 +0600

    uploading setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='hello-world',
+    version='1.0.0',
+    py_modules=['hello'],
+    author='Your Name',
+    author_email='your@email.com',
+    description='A simple "Hello World" package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
